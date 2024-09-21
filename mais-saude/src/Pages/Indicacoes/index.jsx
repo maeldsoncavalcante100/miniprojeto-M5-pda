be2393114f04a3621667718759cbc8fc56383b49
Maeldson Cavalcante <maeldson2015@gmail.com>
2024-09-21 16:34:23 -0300

    :wrench: feat: Making connection with API

--- mais-saude/src/Pages/Indicacoes/index.jsx
+++ mais-saude/src/Pages/Indicacoes/index.jsx
@@ -1,19 +1,38 @@
+import axios from 'axios'
+import { useEffect, useState } from "react";
 import Card from "../../components/Cards";
 import styles from "./Indicacoes.module.css";
 
 function Indicacoes() {
+  const [livros, setLivros] = useState([]);
+
+  useEffect(() => {
+    const buscarLivros = async () => {
+      try {
+        const response = await axios.get(
+          "https://projeto-final-m4-pda.onrender.com/livros"
+        );
+        setLivros(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar os repositórios:", error);
+      }
+    };
+
+    buscarLivros();
+  }, []);
+
   return (
     <section className={styles.indicacoes}>
       <h2>Indicações de leitura</h2>
-      {repositories.length > 0 ? (
+      {livros.length > 0 ? (
         <section className={styles.lista}>
-          {repositories.map((repo) => (
+          {livros.map((liv) => (
             <Card
-              key={repo.id}
-              name={repo.name}
-              description={repo.description}
-              price={repo.price}
-              link={repo.link}
+              key={liv.id}
+              name={liv.name}
+              description={liv.description}
+              price={liv.price}
+              link={liv.link}
             />
           ))}
         </section>
